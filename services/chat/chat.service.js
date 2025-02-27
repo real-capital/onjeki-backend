95fece59f0a8a4ea94b6a643c8d8d04c3d3358e5
creedtech <ayodeleayoolaoke4@gmail.com>
2025-02-27 11:28:58 +0000

    refactor: add logging for user activity and message emissions in ChatService

--- services/chat/chat.service.js
+++ services/chat/chat.service.js
@@ -127,11 +127,13 @@ class ChatService {
     });
 
     activeChats.forEach((chat) => {
+      console.log(`user active ${chat.id}`);
       socket.join(`chat_${chat._id}`);
     });
   }
 
   handleJoinChat(socket, chatId) {
+    console.log(`user joining ${chat.id}`);
     socket.join(`chat_${chatId}`);
   }
 
@@ -168,6 +170,7 @@ class ChatService {
       };
       await chat.save();
 
+      console.log(`emitting messages ${newMessage}`);
       // Emit message to all participants in the chat room
       this.io.to(`chat_${chatId}`).emit('new_message', {
         chatId,
@@ -324,83 +327,3 @@ class ChatService {
 }
 
 export default ChatService;
-
-// class ChatService {
-//   constructor(io) {
-//     this.io = io;
-//     this.setupSocketHandlers();
-//   }
-
-//   setupSocketHandlers() {
-//     this.io.on('connection', (socket) => {
-//       socket.on('join-chat', (chatId) => {
-//         socket.join(chatId);
-//       });
-
-//       socket.on('send-message', async (messageData) => {
-//         const message = await this.saveMessage(messageData);
-//         this.io.to(messageData.chatId).emit('new-message', message);
-//       });
-
-//       socket.on('typing', (data) => {
-//         socket.to(data.chatId).emit('user-typing', data.userId);
-//       });
-//     });
-//   }
-
-//   async createChat(userId1, userId2, propertyId) {
-//     const existingChat = await ChatModel.findOne({
-//       users: { $all: [userId1, userId2] },
-//       property: propertyId
-//     });
-
-//     if (existingChat) {
-//       return existingChat;
-//     }
-
-//     const newChat = await ChatModel.create({
-//       users: [userId1, userId2],
-//       property: propertyId
-//     });
-
-//     return newChat;
-//   }
-
-//   async saveMessage(messageData) {
-//     const message = await MessageModel.create({
-//       chat: messageData.chatId,
-//       sender: messageData.senderId,
-//       content: messageData.content,
-//       type: messageData.type // text, image, etc.
-//     });
-
-//     await ChatModel.findByIdAndUpdate(messageData.chatId, {
-//       lastMessage: message._id,
-//       $inc: { messageCount: 1 }
-//     });
-
-//     return message;
-//   }
-
-//   async getChatHistory(chatId, pagination) {
-//     const { page = 1, limit = 50 } = pagination;
-//     const messages = await MessageModel.find({ chat: chatId })
-//       .sort({ createdAt: -1 })
-//       .skip((page - 1) * limit)
-//       .limit(limit)
-//       .populate('sender', 'name');
-
-//     return messages.reverse();
-//   }
-
-//   async getUserChats(userId) {
-//     return ChatModel.find({ users: userId })
-//       .populate('users', 'name')
-//       .populate('property', 'title photos')
-//       .populate('lastMessage');
-//   }
-// }
-
-// export default ChatService;
-
-// services/chat.service.js
