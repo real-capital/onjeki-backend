9314f8d55f5b7743a0f046ad34f5fd18aec9ed0d
creedtech <ayodeleayoolaoke4@gmail.com>
2025-05-13 15:43:10 +0100

    fix: correct import path for RentSalesChatController to match file naming convention

--- routes/rentSalesChat/rentSalesChat.route.js
+++ routes/rentSalesChat/rentSalesChat.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { Route } from '../../interfaces/route.interface.js';
 import { isAuthenticated } from '../../middlewares/auth.js';
-import RentSalesChatController from '../../controller/conversation/rentSalesChat.Controller.js';
+import RentSalesChatController from '../../controller/conversation/rentsaleschat.controller.js';
 
 class RentSalesChatRoute extends Route {
   constructor() {
