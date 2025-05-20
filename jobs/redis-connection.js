3b811b175fa8b5fc70d8baa299a3f03b1dffd174
creedtech <ayodeleayoolaoke4@gmail.com>
2025-05-20 18:41:19 +0100

    Enable offline queue for Redis connection to improve job handling

--- jobs/redis-connection.js
+++ jobs/redis-connection.js
@@ -101,8 +101,8 @@ export const redisConnection = new IORedis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
   maxRetriesPerRequest: null,
-  enableOfflineQueue: false,
-  offlineQueue: false,
+  enableOfflineQueue: true,
+  offlineQueue: true,
 });
 
 redisConnection.on('connect', () => {
