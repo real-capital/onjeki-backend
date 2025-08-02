28c08ac512a09904ff76ffd6120d042d51cf6c8c
creedtech <ayodeleayoolaoke4@gmail.com>
2025-08-02 22:41:55 +0100

    removed rate limit

--- app.js
+++ app.js
@@ -354,12 +354,12 @@ class App {
     this.app.use(morganMiddleware);
 
     // Rate limiting
-    const limiter = rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-      message: 'Too many requests from this IP, please try again later.',
-    });
-    this.app.use('/api/', limiter);
+    // const limiter = rateLimit({
+    //   windowMs: 15 * 60 * 1000, // 15 minutes
+    //   max: 100, // limit each IP to 100 requests per windowMs
+    //   message: 'Too many requests from this IP, please try again later.',
+    // });
+    // this.app.use('/api/', limiter);
 
     logger.info('✅ Middlewares initialized');
   }
